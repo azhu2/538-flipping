11fb31cd1efa17ac186e64f65d120558168ea83b
Alex Zhu <alex.t.zhu@gmail.com>
2020-03-05 02:42:28 -0800

    Clean up python mess

--- solve.py
+++ solve.py
@@ -1,108 +1,101 @@
 #!/usr/bin/python
 
 from __future__ import division
-import sys
+import argparse
 
-# TODO make flag
-DEBUG = False
+class Solver:
+    def __init__(self, iterations):
+        self.row_count = int(iterations) + 1
+        self.min_value = -2 * self.row_count
+        self.max_value = self.row_count + 1
 
-# TODO Ew globals
-ROW_COUNT = 0
-MIN_VALUE = 0
-MAX_VALUE = 0
+        # Init empty table of Nones
+        self.data_table = [[None for _ in range(self.max_value - self.min_value + 1)] for _ in range(self.row_count + 1)]
+        # Seed value for starting at 0
+        self.data_table[0][self.getIndexForValue(0)] = 1
 
-def main(argv = None):
-    if argv is None:
-        argv = sys.argv
+    def populate_table(self):
+        for value in range(self.min_value, self.max_value + 1):
+            self.getOrCalculateValue(self.row_count, value)
 
-    try:
-        global ROW_COUNT
-        ROW_COUNT = int(argv[1]) + 1
-        # Columns go from -2 * ROW_COUNT to ROW_COUNT, use getIndexForValue() to convert from value to column index
-        global MIN_VALUE
-        MIN_VALUE = -2 * ROW_COUNT
-        global MAX_VALUE
-        MAX_VALUE = ROW_COUNT + 1
-    except:
-        sys.exit('Invalid or missing flip count')
+    def getOrCalculateValue(self, row, value):
+        colIndex = self.getIndexForValue(value);
 
-    data_table = init_table()
+        curValue = self.data_table[row][colIndex]
+        if curValue is not None:
+            return curValue
 
-    populateTable(data_table)
+        contributions = []
 
-    if DEBUG:
-        print_debug_table(data_table)
+        # Edge case; row 0 is all 0 except for seed value
+        if row != 0:
+            # Left side contribution
+            if value > self.min_value + 1:
+                # Coin B - contributions from range(..0) map to range(..2)
+                if value <= 2:
+                    contributions.append(self.getOrCalculateValue(row - 1, value - 2))
+                # Coin A - contributions from range(1..) map to range(2..)
+                if value >= 2:
+                    contributions.append(self.getOrCalculateValue(row - 1, value - 1))
+            # Right side contribution
+            if value < self.max_value:
+                # Coin B - contributions from range(..0) map to range(..-2)
+                if value <= -2:
+                    contributions.append(self.getOrCalculateValue(row - 1, value + 2))
+                # Coin B - contributions from range(1..) map to range(0..)
+                if value >= 0:
+                    contributions.append(self.getOrCalculateValue(row - 1, value + 1))
 
-    # Print results
-    print('\n\n{:>5} | {:>12} | {:>12} | {}'.format('Flips', 'Wins', 'Permutations', 'Win Ratio'))
-    for row in range(ROW_COUNT):
-        win_count = getWinCount(data_table, row)
-        total_possibilities = pow(2, row)
-        win_ratio = win_count / total_possibilities
+        result = sum(contributions)
+        self.data_table[row][colIndex] = result
+        return result
 
-        print('{:5d} | {:12d} | {:12d} | ({:.4%})'.format(row, win_count, total_possibilities, win_ratio))
+    # Convert column label to array column index
+    def getIndexForValue(self, value):
+        return value - self.min_value
 
-def init_table():
-    data_table = [[None for _ in range(MAX_VALUE - MIN_VALUE + 1)] for _ in range(ROW_COUNT + 1)]
+    def print_results(self, debug):
+        if debug:
+            self.print_debug_table()
 
-    # Seed value for starting at 0
-    data_table[0][getIndexForValue(0)] = 1
-    return data_table
+        print '-------------------- Results --------------------'
+        print('{:>5} | {:>32} | {:>32} | {}'.format('Flips', 'Wins', 'Permutations', 'Win Ratio'))
+        for row in range(self.row_count):
+            win_count = self.get_win_count(row)
+            total_possibilities = pow(2, row)
+            win_ratio = win_count / total_possibilities
 
-def populateTable(data_table):
-    for value in range(MIN_VALUE, MAX_VALUE + 1):
-        getOrCalculateValue(data_table, ROW_COUNT, value)
+            print('{:5d} | {:32d} | {:32d} | {:.4%}'.format(row, win_count, total_possibilities, win_ratio))
 
-def getOrCalculateValue(data_table, row, value):
-    colIndex = getIndexForValue(value);
-
-    curValue = data_table[row][colIndex]
-    if curValue is not None:
-        return curValue
-
-    contributions = []
-
-    # Edge case; row 0 is all 0 except for seed value
-    if row != 0:
-        # Left side contribution
-        if value > MIN_VALUE + 1:
-            # Coin B - contributions from range(..0) map to range(..2)
-            if value <= 2:
-                contributions.append(getOrCalculateValue(data_table, row - 1, value - 2))
-            # Coin A - contributions from range(1..) map to range(2..)
-            if value >= 2:
-                contributions.append(getOrCalculateValue(data_table, row - 1, value - 1))
-        # Right side contribution
-        if value < MAX_VALUE:
-            # Coin B - contributions from range(..0) map to range(..-2)
-            if value <= -2:
-                contributions.append(getOrCalculateValue(data_table, row - 1, value + 2))
-            # Coin B - contributions from range(1..) map to range(0..)
-            if value >= 0:
-                contributions.append(getOrCalculateValue(data_table, row - 1, value + 1))
-
-    result = sum(contributions)
-    data_table[row][colIndex] = result
-    return result
-
-def print_debug_table(data_table):
-    print 'Flips | ',
-    for col_label in range(MIN_VALUE, MAX_VALUE + 1):
-        print '{:5d}'.format(col_label),
-    print
-    for row_num, row in enumerate(data_table):
-        print '{:5d} | '.format(row_num),
-        for item in row:
-            print '{:5d}'.format(item),
+    def print_debug_table(self):
+        print '-------------------- Debug Table --------------------'
+        print 'Flips | ',
+        for col_label in range(self.min_value, self.max_value + 1):
+            print '{:4d}'.format(col_label),
         print
+        for row_num, row in enumerate(self.data_table):
+            print '{:5d} | '.format(row_num),
+            for item in row:
+                print '{:4d}'.format(item),
+            print
+        print '\n'
 
-def getWinCount(data_table, row):
-    # Sum values in row from 1 to end
-    return sum(data_table[row][getIndexForValue(1):])
+    def get_win_count(self, row):
+        # Sum values in row from 1 to end
+        return sum(self.data_table[row][self.getIndexForValue(1):])
 
-# Convert column label to array column index
-def getIndexForValue(value):
-    return value - MIN_VALUE
+def main(iterations, debug):
+    solver = Solver(iterations)
+    solver.populate_table()
+    solver.print_results(debug)
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('iterations', help = 'number of coin flips')
+    parser.add_argument('--debug', help = 'print table of permutations (not recommended for iterations > 15)', action = 'store_true')
+    args = parser.parse_args()
+    return args.iterations, args.debug
 
 if __name__ == '__main__':
-    main();
+    args = parseArgs()
+    main(*args);
